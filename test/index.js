cee79ea399efb0d60c4a6b4d6444d1e5eed7b61d
James Swift <james.rt.swift@gmail.com>
2016-06-26 15:35:37 +0100

    Testing Travis CI integration

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 var scon = require('../index.js');
 
-describe("Swift Binary Tag file format", function() {
+describe("Swift-Cardinal Object Notation file format", function() {
 	
 	it("should write the magic number", function(){
 		expect(scon.encode( {} ).result.substring( 0, scon.magicNumber.length ) ).to.equal( scon.magicNumber );
