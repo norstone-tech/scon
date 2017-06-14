6afbe8561b9cd24b85a99b05eff69b58def4c787
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2017-06-14 15:15:21 -0400

    Fixed more issues caused by PR Fixed partial decoding not working properly

--- example.js
+++ example.js
@@ -8,10 +8,55 @@
 
 "use strict";
 
+var charCodeSymbols = [
+"NUL",
+"SOH",
+"STX",
+"ETX",
+"EOT",
+"ENQ",
+"ACK",
+"BEL",
+"BS",
+"TAB",
+"LF",
+"VT",
+"FF",
+"CR",
+"SO",
+"SI",
+"DLE",
+"DC1",
+"DC2",
+"DC3",
+"DC4",
+"NAK",
+"SYN",
+"ETB",
+"CAN",
+"EM",
+"SUB",
+"ESC",
+"FS",
+"GS",
+"RS",
+"US",
+"space"
+];
+var debugArr = function(arr){
+	console.log("Stuff:")
+	for(let i=0;i<arr.length;i+=1){
+		console.log(i+": "+arr[i]+" "+(charCodeSymbols[arr[i]] || String.fromCharCode(arr[i])))
+	}
+}
+
+
+
 var scon = require( "./index.js" );
 
-var encoded = scon.encode( { hello: "world!!", hi: "bye", five: NaN, pi: 3.14159, object:{amazing:true,true:{"mind":"fuck"}}, six: 6 ,arr:["wan","too","free",{"for":4},[1,2,3,4,5]]});
-console.log( "encoded:", encoded );
+var encoded = scon.encode( { testInt:8 ,hello: "world!!", hi: "bye", five: NaN, pi: 3.14159, object:{amazing:true,true:{"mind":"fuck"}}, six: 6 ,arr:["wan","too","free",{"for":4},[1,2,3,4,5]]});
+console.log( "encoded:" );
+debugArr(encoded)
 
-var decoded = scon.decode( encoded.result );
-console.log( "decoded:", decoded.result);
+var decoded = scon.decode( encoded );
+console.log( "decoded:", decoded);
