27ed654185c0e8ee055fe9626cce36cab45bc1a4
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2021-11-02 10:34:37 -0400

    more performance improvments, remove old tests

--- quicktest.js
+++ quicktest.js
@@ -1,3 +1,5 @@
+const Benchmark = require('benchmark');
+const suite = new Benchmark.Suite("SCON/JSON comparison");
 const {SconEncoder, SconDecoder} = require("./");
 const se = new SconEncoder({referencedStrings: false});
 const sd = new SconDecoder();
@@ -11,28 +13,30 @@ const testVal = {
 for(let i = 0; i < 1000; i += 1){
 	// testVal[i.toString(36)] = Math.random() * 10000 | 0;
 	// testVal[i.toString(36)] = Math.random() * 10000;
-	testVal[i.toString(36)] = Math.random().toString(36);
+	// testVal[i.toString(36)] = Math.random().toString(36);
 	// testVal[(i + 1000).toString(36)] = "test";
 }
 const encodedJson = JSON.stringify(testVal);
 const encodedScon = se.encode(testVal);
-console.time("JSON Encode");
-for(let i = 0; i < testCount; i += 1){
+suite.add("JSON Encode", function() {
 	const something = JSON.stringify(testVal);
-}
-console.timeEnd("JSON Encode");
-console.time("SCON Encode");
-for(let i = 0; i < testCount; i += 1){
-	const something = se.encode(testVal);
-}
-console.timeEnd("SCON Encode");
-console.time("JSON Decode");
-for(let i = 0; i < testCount; i += 1){
+});
+suite.add("SCON Encode", function() {
+	const something = se.encode(testVal)
+});
+
+suite.add("JSON Decode", function() {
 	const something = JSON.parse(encodedJson);
-}
-console.timeEnd("JSON Decode");
-console.time("SCON Decode");
-for(let i = 0; i < testCount; i += 1){
+});
+suite.add("SCON Decode", function() {
 	const something = sd.decode(encodedScon);
-}
-console.timeEnd("SCON Decode");
+});
+// add listeners
+suite.on('cycle', function(event) {
+	console.log(String(event.target));
+})
+.on('complete', function() {
+console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async
+.run({async: true, minTime: 30, minSamples: 100});
