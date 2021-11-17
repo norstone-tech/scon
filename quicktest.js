24baefb36e834f194b245524b19a6156b0a2899c
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2021-11-16 21:37:07 -0500

    benchmarking

--- quicktest.js
+++ quicktest.js
@@ -2,8 +2,8 @@
 const Benchmark = require("benchmark");
 const suite = new Benchmark.Suite("SCON/JSON comparison");
 const {FastSconEncoder, SconEncoder, SconDecoder} = require("./");
-const se = new SconEncoder();
-const fse = new SconEncoder();
+const se = new SconEncoder({referencedStrings: true});
+const fse = new FastSconEncoder();
 const sd = new SconDecoder();
 const testVal = {
 	hello: "world",
@@ -11,16 +11,46 @@ const testVal = {
 	number: 1337,
 	float: 13.37
 };
-
+const testCount = 100;
 for(let i = 0; i < 500; i += 1){
-	testVal[i.toString(36)] = Math.random() * 10000 | 0;
-	// testVal[i.toString(36)] = Math.random();
-	 testVal[(i + 500).toString(36)] = Math.random().toString(36);
+	// testVal[i.toString(36)] = Math.random() * 10000 | 0;
+	testVal[i.toString(36)] = Math.random();
+	// testVal[(i + 500).toString(36)] = Math.random().toString(36);
 	// testVal[(i + 1000).toString(36)] = "test";
 }
 
 const encodedJson = JSON.stringify(testVal);
 const encodedScon = se.encode(testVal);
+
+console.time("JSON Encode");
+for(let i = 0; i < testCount; i += 1){
+	const something = JSON.stringify(testVal);
+}
+console.timeEnd("JSON Encode");
+console.time("SCON Encode");
+for(let i = 0; i < testCount; i += 1){
+	const something = se.encode(testVal);
+}
+console.timeEnd("SCON Encode");
+console.time("SCON Encode (fast)");
+for(let i = 0; i < testCount; i += 1){
+	const something = fse.encode(testVal);
+}
+console.timeEnd("SCON Encode (fast)");
+
+console.time("JSON Decode");
+for(let i = 0; i < testCount; i += 1){
+	const something = JSON.parse(encodedJson);
+}
+console.timeEnd("JSON Decode");
+console.time("SCON Decode");
+for(let i = 0; i < testCount; i += 1){
+	const something = sd.decode(encodedScon);
+}
+console.timeEnd("SCON Decode");
+if(process.argv.includes("quick")){
+	return;
+}
 suite.add("JSON Encode", () => {
 	// eslint-disable-next-line no-unused-vars
 	const something = JSON.stringify(testVal);
